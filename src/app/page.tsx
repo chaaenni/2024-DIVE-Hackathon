b4106be7dad80881d29b84ca95939a019df8ece1
chaaaeyeon <jcy2749@gmail.com>
2024-10-04 03:12:51 +0900

    load google maps api to nextJS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <div className="bg-white">
-      <span>hello 채연!</span>
+    <div className="bg-white px-8 py-10">
+      <h2 className="text-3xl font-bold mb-10">GONZI GONZI</h2>
+      <a href="recommend" className="underline">컨셉 별 워케이션 라이프 스타일 추천</a>
     </div>
   );
 }
---
+++ src/app/recommend/page.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+import React from 'react'
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
+
+interface mapContainerStyle{
+    width: string;
+    height: string;
+}
+
+export default function Recommend(){
+    const containerStyle:mapContainerStyle = {
+        width: '700px',
+        height: '900px'
+    }
+    const center = {
+        lat: 35.14246667, //latitude(위도)
+        lng: 129.115375, //longitude(경도)
+    }
+    return(
+        <div>
+            <LoadScript googleMapsApiKey={process.env.GOOGLE_MAPS_API_KEY as string}>
+                <GoogleMap 
+                    mapContainerStyle={containerStyle}
+                    center={center}
+                    zoom={12}
+                >
+
+                </GoogleMap>
+            </LoadScript>
+        </div>
+    )
+}
